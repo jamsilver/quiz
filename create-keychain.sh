ead9dc81401bd34d8974dc6fe49b25c543706769
James Silver <jamsilver@mailfence.com>
2018-07-22 15:45:57 +0100

    Fix typos

--- create-keychain.sh
+++ create-keychain.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
 # This should be passed in via travis encrypt mechanism.
-"${KEY_PASSWORD:?Missing KEY_PASSWORD environment variable}"
+: "${KEY_PASSWORD:?Missing KEY_PASSWORD environment variable}"
 
 # Generate password for a temporary keychain.
-KEYCHAIN_PASSWORD=`head -c32 < /dev/urandom | base64`
+KEYCHAIN_PASSWORD="`head -c32 < /dev/urandom | base64`"
 
 # Create the keychain with a password.
-security create-keychain -p $KEYCHAIN_PASSWORD ios-build.keychain
+security create-keychain -p "$KEYCHAIN_PASSWORD" ios-build.keychain
 
 # Make the custom keychain default, so xcodebuild will use it for signing.
 security default-keychain -s ios-build.keychain
 
 # Unlock the keychain
-security unlock-keychain -p $KEYCHAIN_PASSWORD ios-build.keychain
+security unlock-keychain -p "$KEYCHAIN_PASSWORD" ios-build.keychain
 
 # Add certificates to keychain and allow codesign to access them.
 security import ./apple.cer -k ~/Library/Keychains/ios-build.keychain -A
 security import ./cert.cer -k ~/Library/Keychains/ios-build.keychain -A
-security import ./key.p12 -k ~/Library/Keychains/ios-build.keychain -P $KEY_PASSWORD -A
+security import ./key.p12 -k ~/Library/Keychains/ios-build.keychain -P "$KEY_PASSWORD" -A
 
-security set-key-partition-list -S apple-tool:,apple:,codesign: -s -k $KEYCHAIN_PASSWORD ~/Library/Keychains/ios-build.keychain
+security set-key-partition-list -S apple-tool:,apple:,codesign: -s -k "$KEYCHAIN_PASSWORD" ~/Library/Keychains/ios-build.keychain
 
